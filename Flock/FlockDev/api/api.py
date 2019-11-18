2c12be02537c716dd07b833eb367bf23861626bc
Aneesh Rastogi <aneeshr@umich.edu>
2019-11-18 16:30:30 -0500

    nicoles sql queries and sql updates

--- Flock/FlockDev/api/api.py
+++ Flock/FlockDev/api/api.py
@@ -1,3 +1,4 @@
+"""API for Flock."""
 from flask import Flask
 import flask
 import hashlib
@@ -9,15 +10,15 @@ import sqlite3
 def login():
   """Login Page."""
   username = flask.request.form['user']
-  unHashedPass = flask.request.form['password']
+  unHashedPass = flask.request.form['pword']
   
   # get db
   database = FlockDev.model.get_db()
   cursor = database.cursor()
 
   #get password from database corresponding to username
-  cursor.execute("SELECT password FROM users WHERE username = ?;",(username,))
-  returnedPassword = cursor.fetchone()["password"]
+  cursor.execute("SELECT pword FROM users WHERE username = ?;",(username,))
+  returnedPassword = cursor.fetchone()["pword"]
 
   context = {}
   
@@ -28,19 +29,25 @@ def login():
   # maybe put session
   return flask.jsonify(**context)
   
-@FlockDev.app.route('/events/<username>/', methods=['GET', 'POST'])
-def availableEvents(username):
+@FlockDev.app.route('/events/<fullname>/', methods=['GET', 'POST'])
+def availableEvents(fullname):
   """Update Events."""
-
   # get db
   database = FlockDev.model.get_db()
   cursor = database.cursor()
 
   # return all the events that a user has not seen
-  eventQuery = "SELECT * FROM events WHERE eventID NOT IN \
-    eventID FROM userSeenEvent WHERE username = ?;"
+  # eventQuery = "SELECT * FROM events WHERE eventID NOT IN \
+  #   eventID FROM userSeenEvent WHERE username = ?;"
+  eventQuery = "CREATE VIEW View_Events (eventID) AS \
+    SELECT DISTINCT eventID FROM events; \
+    SELECT DISTINCT eventID FROM View_Events \
+    MINUS \
+    SELECT DISTINCT VE.eventID \
+    FROM userEventInfo UEI, View_Events VE \
+    WHERE UEI.eventID == VE.eventID AND UEI.userID == " + fullname + ";"
   
-  context = cursor.execute(eventQuery, (username,))
+  context = cursor.execute(eventQuery, (fullname,))
 
   return flask.jsonify(**context)
 
@@ -49,7 +56,6 @@ def availableEvents(username):
                     methods = ['GET','POST'])
 def interestedEvents(username, eventID):
   """Update the table to reflect a user has seen and is interested."""
-
   # get db
   database = FlockDev.model.get_db()
   cursor = database.cursor()
@@ -67,11 +73,12 @@ def interestedEvents(username, eventID):
                     methods = ['GET', 'POST'])
 def notInterestedEvents(username, eventID):
   """Update the table to reflect a user has seen and is not interested."""
-
   # get db
   database = FlockDev.model.get_db()
   cursor = database.cursor()
 
   # insert into seen
-  seenQuery = "INSERT INTO userSeenEvent(eventId, username) VALUES(?,?)";
-  cursor.execute(seenQuery, (username, eventID))
+  seenQuery = "INSERT INTO userSeenEvent(username, eventID)VALUES(?,?);"
+  database.execute(seenQuery, (username, int(eventID)))
+
+  return "executed"
