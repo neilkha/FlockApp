7adf44f02b481aa0a5d5a49415507ca79e7fd7b6
Aneesh Rastogi <aneeshr@umich.edu>
2019-11-19 14:01:50 -0500

    nicole plz fix schema and data.sql

--- Flock/sql/data.sql
+++ Flock/sql/data.sql
@@ -2,6 +2,7 @@ INSERT INTO users(userID, fullname, email, pword, phone, picture, tagID)
 VALUES (1, 'Michael Cafarella', 'michjc@umich.edu', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8', 2483124234, NULL, 1),
 (2, 'H.V. Jagadish', 'jag@umich.edu', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8', 2483124234, NULL, 2),
 (3, 'Andrew DeOrio', 'jag@umich.edu', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8', 2483124234, NULL, 3);
+(4, 'Neil Khandawala', 'chessmaster333@gmail.com', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8', 2483124234, NULL, 7);
 
 INSERT INTO tags(tagID, outdoor_adventures, cooking, gaming, night_life, swimming, weight_lifting, photography, yoga, basketball, dancing)
 VALUES (1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1),
@@ -9,7 +10,8 @@ VALUES (1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1),
 (3, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0),
 (4, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0),
 (5, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0),
-(6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+(6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 
 INSERT INTO events(eventID, eventName, host, phone, picture, tagID)
 VALUES (1, 'Hiking with Friends', 'deorio', 2483124234, NULL, 4),
@@ -26,16 +28,3 @@ VALUES (1, 1, 2),
 (3, 1, 0),
 (2, 2, 0),
 (2, 3, 0);
-
-INSERT INTO eventAttendees(eventID, username)
-VALUES (1, 'michjc'),
-(1, 'jag'),
-(2, 'deorio');
-
-INSERT INTO userSeenEvent(username, eventID)
-VALUES('michjc', 1),
-('jag', 1),
-('deorio', 2),
-('deorio', 1),
-('jag', 2),
-('jag', 3);
