e41598b86a569dd30bdb3a26aa5c1e869cfdd88f
Nicole Feygin <nfeygin@umich.edu>
2019-11-19 20:32:06 -0500

    made api to get events of certain status

--- Flock/FlockDev/api/api.py
+++ Flock/FlockDev/api/api.py
@@ -37,16 +37,11 @@ def availableEvents(beginEmail, endEmail):
   cursor = database.cursor()
 
   email = beginEmail + '@' + endEmail
-  # return all the events that a user has not seen
-  # eventQuery = "SELECT * FROM events WHERE eventID NOT IN \
-  #   eventID FROM userSeenEvent WHERE username = ?;"
 
+  # return all the events that a user has not seen
   eventQuery = "CREATE VIEW View_Events (eventID) AS \
     SELECT DISTINCT eventID FROM events;"
-
   context = cursor.execute(eventQuery)
-
-  
 
   eventQuery = "SELECT DISTINCT eventID FROM View_Events \
     EXCEPT \
@@ -71,22 +66,60 @@ def availableEvents(beginEmail, endEmail):
   return flask.jsonify(**eventInfo)
 
 
-@FlockDev.app.route('/events/Interested/<username>/<int:eventID>',
-                    methods = ['GET','POST'])
-def interestedEvents(username, eventID):
+@FlockDev.app.route('/events/getStatusEvents/<beginEmail>/<endEmail>/<status>',
+                    methods = ['GET'])
+def getStatusEvents(beginEmail, endEmail, status):
+  """View what events a user is interested in"""
+  # get db
+  database = FlockDev.model.get_db()
+  cursor = database.cursor()
+
+  email = beginEmail + '@' + endEmail
+
+  eventQuery = "SELECT DISTINCT UEI.eventID \
+                FROM users U, userEventInfo UEI \
+                WHERE U.email = ? AND U.userID = UEI.userID \
+                AND UEI.commitStatus = ?"
+  context = cursor.execute(eventQuery, (email, status,)).fetchall()
+
+  eventInfo = {}
+  i = 0
+  for element in context:
+    eventID = element['eventID']
+    query = "SELECT eventID, eventName, eventDescription, picture FROM EVENTS WHERE eventID == " + str(eventID) + ";"
+    eventInfo[str(i)] = cursor.execute(query).fetchone()
+    i += 1
+  
+  return flask.jsonify(**eventInfo)
+
+
+@FlockDev.app.route('/events/postEventStatus/<userID>/<int:eventID>/<int:status>',
+                    methods = ['POST'])
+def postEventStatus(userID, eventID, status):
   """Update the table to reflect a user has seen and is interested."""
   # get db
   database = FlockDev.model.get_db()
   cursor = database.cursor()
 
-  # insert into seen
-  seenQuery = "INSERT INTO userSeenEvent(eventId, username) VALUES(?,?);"
-  cursor.execute(seenQuery, (username, eventID))
-  # insert into attending
-  eventAttendeesQuery  = "INSERT INTO eventAttendees(eventID, username)\
-     VALUES(?,?);"
-  cursor.execute(eventAttendeesQuery, (username, eventID))
+  # email = beginEmail + '@' + endEmail
 
+  # # grab the userID using the user email
+  # userQuery = "SELECT userID FROM users \
+  #              WHERE users.email = ?;"
+  # context = cursor.execute(userQuery, (email,)).fetchall()
+  # return flask.jsonify(**context)
+
+  # insert into userEventInfo table that user is interested
+  eventQuery = "INSERT INTO userEventInfo (userID, eventID, commitStatus) \
+                VALUES(?,?,?);"
+  cursor.execute(eventQuery, (userID, eventID, status))
+
+  context = {}
+  context['message'] = "Uploaded"
+  context['status_code'] = 200
+  
+  return flask.jsonify(**context)
+  
 
 @FlockDev.app.route('/events/notInterestedEvents/<username>/<int:eventID>/',
                     methods = ['GET', 'POST'])
--- Flock/sql/data.sql
+++ Flock/sql/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO users(userID, fullname, email, pword, phone, picture, tagID)
 VALUES (1, 'Michael Cafarella', 'michjc@umich.edu', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8', 2483124234, NULL, 1),
 (2, 'H.V. Jagadish', 'jag@umich.edu', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8', 2483124234, NULL, 2),
-(3, 'Andrew DeOrio', 'jag@umich.edu', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8', 2483124234, NULL, 3),
+(3, 'Andrew DeOrio', 'adeorio@umich.edu', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8', 2483124234, NULL, 3),
 (4, 'Neil Khandawala', 'chessmaster333@gmail.com', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8', 2483124234, NULL, 7);
 
 INSERT INTO tags(tagID, outdoor_adventures, cooking, gaming, night_life, swimming, weight_lifting, photography, yoga, basketball, dancing)
